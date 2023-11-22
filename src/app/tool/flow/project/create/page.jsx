2e6ab553eef0ae906ee3a49c771d816c4ac5f365
Databayt <osmanabdout@hotmail.com>
2023-11-23 01:36:41 +0300

    bypass

--- src/app/tool/flow/project/create/page.jsx
+++ src/app/tool/flow/project/create/page.jsx
@@ -1,67 +1,67 @@
-"use client";
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+// "use client";
+// import React, { useState } from "react";
+// import { useRouter } from "next/navigation";
 
-const Create = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+// const Create = () => {
+//   const [title, setTitle] = useState("");
+//   const [description, setDescription] = useState("");
 
-  const router = useRouter();
+//   const router = useRouter();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
 
-    if (!title || !description) {
-      alert("Title and description are required.");
-      return;
-    }
+//     if (!title || !description) {
+//       alert("Title and description are required.");
+//       return;
+//     }
 
-    try {
-      const res = await fetch("https://databayt.org/api/topics", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({ title, description }),
-      });
+//     try {
+//       const res = await fetch("https://databayt.org/api/topics", {
+//         method: "POST",
+//         headers: {
+//           "Content-type": "application/json",
+//         },
+//         body: JSON.stringify({ title, description }),
+//       });
 
-      if (res.ok) {
-        router.push("/tool/flow/project");
-      } else {
-        throw new Error("Failed to create a topic");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-    <input
-      onChange={(e) => setTitle(e.target.value)}
-      value={title}
-      className="border border-slate-500 px-8 py-2"
-      type="text"
-      placeholder="Title"
-    />
+//       if (res.ok) {
+//         router.push("/tool/flow/project");
+//       } else {
+//         throw new Error("Failed to create a topic");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   return (
+//     <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+//     <input
+//       onChange={(e) => setTitle(e.target.value)}
+//       value={title}
+//       className="border border-slate-500 px-8 py-2"
+//       type="text"
+//       placeholder="Title"
+//     />
 
-    <input
-      onChange={(e) => setDescription(e.target.value)}
-      value={description}
-      className="border border-slate-500 px-8 py-2"
-      type="text"
-      placeholder="Description"
-    />
+//     <input
+//       onChange={(e) => setDescription(e.target.value)}
+//       value={description}
+//       className="border border-slate-500 px-8 py-2"
+//       type="text"
+//       placeholder="Description"
+//     />
 
-    <button
-      type="submit"
-      className="bg-green-600  text-white py-3 px-6 w-fit"
-    >
-      Create project
-    </button>
-  </form>
-  );
-};
+//     <button
+//       type="submit"
+//       className="bg-green-600  text-white py-3 px-6 w-fit"
+//     >
+//       Create project
+//     </button>
+//   </form>
+//   );
+// };
 
-export default Create;
+// export default Create;
 
 
--- src/components/Tool/Flow/Project/EditProject.jsx
+++ src/components/Tool/Flow/Project/EditProject.jsx
@@ -1,57 +1,57 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-export default function EditProject({ id, title, description }) {
-  const [newTitle, setNewTitle] = useState(title);
-  const [newDescription, setNewDescription] = useState(description);
+// import { useState } from "react";
+// import { useRouter } from "next/navigation";
+// export default function EditProject({ id, title, description }) {
+//   const [newTitle, setNewTitle] = useState(title);
+//   const [newDescription, setNewDescription] = useState(description);
 
-  const router = useRouter();
+//   const router = useRouter();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
 
-    try {
-      const res = await fetch(`https://databayt.org/api/topics/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({ newTitle, newDescription }),
-      });
+//     try {
+//       const res = await fetch(`https://databayt.org/api/topics/${id}`, {
+//         method: "PUT",
+//         headers: {
+//           "Content-type": "application/json",
+//         },
+//         body: JSON.stringify({ newTitle, newDescription }),
+//       });
 
-      if (!res.ok) {
-        throw new Error("Failed to update topic");
-      }
+//       if (!res.ok) {
+//         throw new Error("Failed to update topic");
+//       }
 
-      router.refresh();
-      router.push("/tool/flow/project");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//       router.refresh();
+//       router.push("/tool/flow/project");
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-      <input
-        onChange={(e) => setNewTitle(e.target.value)}
-        value={newTitle}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Title"
-      />
+//   return (
+//     <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+//       <input
+//         onChange={(e) => setNewTitle(e.target.value)}
+//         value={newTitle}
+//         className="border border-slate-500 px-8 py-2"
+//         type="text"
+//         placeholder="Title"
+//       />
 
-      <input
-        onChange={(e) => setNewDescription(e.target.value)}
-        value={newDescription}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Description"
-      />
+//       <input
+//         onChange={(e) => setNewDescription(e.target.value)}
+//         value={newDescription}
+//         className="border border-slate-500 px-8 py-2"
+//         type="text"
+//         placeholder="Description"
+//       />
 
-      <button className="bg-black  text-white py-3 px-6 w-fit">
-        Update project
-      </button>
-    </form>
-  );
-}
+//       <button className="bg-black  text-white py-3 px-6 w-fit">
+//         Update project
+//       </button>
+//     </form>
+//   );
+// }
