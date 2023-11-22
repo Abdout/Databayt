652eb8d5219b6b444cb8ae5362eb66cf419247d6
Databayt <osmanabdout@hotmail.com>
2023-11-22 19:00:12 +0300

    try agin

--- src/app/tool/fix/test/page.tsx
+++ src/app/tool/fix/test/page.tsx
@@ -1,11 +1,4 @@
-import NanoIcon from "@/components/Atom/Icon/Nano";
-import CentiInupt from "@/components/Atom/Input/Centi";
-import Burger from "@/components/Home/Burger";
-import Menu from "@/components/Nav/Menu";
-import Side from "@/components/Nav/Side";
-import CentiText from "@/components/Atom/Text/Centi";
-import { testLink } from "@/constants/link";
-import Link from "next/link";
+
 import React from "react";
 
 const Test = () => {
