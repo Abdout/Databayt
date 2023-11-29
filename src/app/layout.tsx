461e0a827cba562b5e9a4707a21ae3f1bccd6f8e
Databayt <osmanabdout@hotmail.com>
2023-11-29 21:38:12 +0300

    D

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
-import "../globals.css";
+import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 
