beabcebaccc20a263789bc1b9c497bfe3c6db5a5
Databayt <osmanabdout@hotmail.com>
2023-11-29 05:50:06 +0300

    8

--- src/app/(root)/tool/flow/layout.tsx
+++ src/app/(root)/tool/flow/layout.tsx
@@ -1,6 +1,5 @@
 import DashHeader from "@/components/tool/flow/DashHeader";
 import DashSide from "@/components/tool/flow/DashSide";
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
