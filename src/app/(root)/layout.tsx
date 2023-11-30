ed5216cff63affb3ac61584ee6344bc3dea5787d
Databayt <osmanabdout@hotmail.com>
2023-11-30 21:25:33 +0300

    l

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,65 +1,13 @@
-// import Header from "@/components/layout/header";
-// import type { Metadata } from "next";
-// import { Inter, Rubik } from "next/font/google";
-
-// const inter = Inter({ subsets: ["latin"], variable: "--inter" });
-// const rubik = Inter({ subsets: ["latin"], variable: "--inter" });
-
-// export const metadata: Metadata = {
-//   title: "Databayt",
-//   description: "business automation",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body className={(inter.variable, rubik.variable)}>
-//         <Header />
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import localFont from "next/font/local";
-import "./globals.css";
 import Header from "@/components/layout/header";
-import Footer from "@/components/layout/footer";
+import type { Metadata } from "next";
+import { Inter, Rubik } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"], variable: "--inter" });
-const rubik = localFont({
-  src: ".../font/rubik.ttf",
-  variable: "--rubik",
-});
-
-const algalam = localFont({
-  src: ".../font/algalam.ttf",
-  variable: "--algalam",
-});
-
-const alvi = localFont({
-  src: ".../font/alvi.ttf",
-  variable: "--alvi",
-});
-
-const riwaj = localFont({
-  src: ".../font/riwaj.ttf",
-  variable: "--riwaj",
-});
-
-const tajawal = localFont({
-  src: "../font/tajawal.ttf",
-  variable: "--tajawal",
-});
+const rubik = Rubik({ subsets: ["latin"], variable: "--inter" });
 
 export const metadata: Metadata = {
   title: "Databayt",
-  description: "Business automation",
+  description: "business automation",
 };
 
 export default function RootLayout({
@@ -69,12 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={(inter.variable, rubik.variable, algalam.variable, alvi.variable, riwaj.variable, tajawal.variable)}>
+      <body className={(inter.variable, rubik.variable)}>
         <Header />
         {children}
-        
       </body>
     </html>
   );
 }
-
