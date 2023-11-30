892724e99378dc0f7a89afa808a2b2a6433f7cc7
Databayt <osmanabdout@hotmail.com>
2023-11-30 21:19:36 +0300

    l

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,65 @@
-import Header from "@/components/layout/header";
+// import Header from "@/components/layout/header";
+// import type { Metadata } from "next";
+// import { Inter, Rubik } from "next/font/google";
+
+// const inter = Inter({ subsets: ["latin"], variable: "--inter" });
+// const rubik = Inter({ subsets: ["latin"], variable: "--inter" });
+
+// export const metadata: Metadata = {
+//   title: "Databayt",
+//   description: "business automation",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={(inter.variable, rubik.variable)}>
+//         <Header />
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
 import type { Metadata } from "next";
-import { Inter, Rubik } from "next/font/google";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
 
 const inter = Inter({ subsets: ["latin"], variable: "--inter" });
-const rubik = Inter({ subsets: ["latin"], variable: "--inter" });
+const rubik = localFont({
+  src: "../font/rubik.ttf",
+  variable: "--rubik",
+});
+
+const algalam = localFont({
+  src: "../font/algalam.ttf",
+  variable: "--algalam",
+});
+
+const alvi = localFont({
+  src: "../font/alvi.ttf",
+  variable: "--alvi",
+});
+
+const riwaj = localFont({
+  src: "../font/riwaj.ttf",
+  variable: "--riwaj",
+});
+
+const tajawal = localFont({
+  src: "../font/tajawal.ttf",
+  variable: "--tajawal",
+});
 
 export const metadata: Metadata = {
   title: "Databayt",
-  description: "business automation",
+  description: "Business automation",
 };
 
 export default function RootLayout({
@@ -17,10 +69,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={(inter.variable, rubik.variable)}>
+      <body className={(inter.variable, rubik.variable, algalam.variable, alvi.variable, riwaj.variable, tajawal.variable)}>
         <Header />
         {children}
+        
       </body>
     </html>
   );
 }
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,53 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import localFont from "next/font/local";
-import "./globals.css";
-import Header from "@/components/layout/header";
-import Footer from "@/components/layout/footer";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import localFont from "next/font/local";
+// import "./globals.css";
+// import Header from "@/components/layout/header";
+// import Footer from "@/components/layout/footer";
 
-const inter = Inter({ subsets: ["latin"], variable: "--inter" });
-const rubik = localFont({
-  src: "../font/rubik.ttf",
-  variable: "--rubik",
-});
+// const inter = Inter({ subsets: ["latin"], variable: "--inter" });
+// const rubik = localFont({
+//   src: "../font/rubik.ttf",
+//   variable: "--rubik",
+// });
 
-const algalam = localFont({
-  src: "../font/algalam.ttf",
-  variable: "--algalam",
-});
+// const algalam = localFont({
+//   src: "../font/algalam.ttf",
+//   variable: "--algalam",
+// });
 
-const alvi = localFont({
-  src: "../font/alvi.ttf",
-  variable: "--alvi",
-});
+// const alvi = localFont({
+//   src: "../font/alvi.ttf",
+//   variable: "--alvi",
+// });
 
-const riwaj = localFont({
-  src: "../font/riwaj.ttf",
-  variable: "--riwaj",
-});
+// const riwaj = localFont({
+//   src: "../font/riwaj.ttf",
+//   variable: "--riwaj",
+// });
 
-const tajawal = localFont({
-  src: "../font/tajawal.ttf",
-  variable: "--tajawal",
-});
+// const tajawal = localFont({
+//   src: "../font/tajawal.ttf",
+//   variable: "--tajawal",
+// });
 
-export const metadata: Metadata = {
-  title: "Databayt",
-  description: "Business automation",
-};
+// export const metadata: Metadata = {
+//   title: "Databayt",
+//   description: "Business automation",
+// };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={(inter.variable, rubik.variable, algalam.variable, alvi.variable, riwaj.variable, tajawal.variable)}>
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={(inter.variable, rubik.variable, algalam.variable, alvi.variable, riwaj.variable, tajawal.variable)}>
         
-        {children}
+//         {children}
         
-      </body>
-    </html>
-  );
-}
+//       </body>
+//     </html>
+//   );
+// }
