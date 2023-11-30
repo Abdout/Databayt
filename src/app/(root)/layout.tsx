f3bf031b966137d4c263de4c2a1c1be7cc7cb25d
Databayt <osmanabdout@hotmail.com>
2023-11-30 21:10:02 +0300

    l

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/layout/header";
 import type { Metadata } from "next";
+import localFont from "next/dist/compiled/@next/font/dist/local";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
+const inter = Inter({ subsets: ["latin"], variable: "--inter" });
+const rubik = localFont({
+  src: "../font/rubik.ttf",
+  variable: "--rubik",
+});
 export const metadata: Metadata = {
   title: "Databayt",
   description: "business automation",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={"{inter.className} "}>
+      <body className={(inter.variable, rubik.variable)}>
         <Header />
         {children}
       </body>
