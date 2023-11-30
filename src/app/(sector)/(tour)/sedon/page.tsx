f60827ecf5232394a575a7480368d47c583b3ef5
Databayt <osmanabdout@hotmail.com>
2023-11-30 19:37:29 +0300

    l

--- src/app/(sector)/(tour)/sedon/page.tsx
+++ src/app/(sector)/(tour)/sedon/page.tsx
@@ -2,8 +2,82 @@ import React from 'react'
 
 const Sedon = () => {
   return (
-    <div>Sedon</div>
-  )
-}
+    <div className=''>
+    <div className="relative bg-gray-100 w-full h-screen overflow-hidden text-left text-[31.05px] text-black font-rubik">
+      <img
+        className="absolute h-[3.68%] w-[6.53%] top-[1.6%] right-[-24.75%] bottom-[94.72%] left-[118.22%] max-w-full overflow-hidden max-h-full object-cover"
+        alt=""
+        src="/instagram-1@2x.png"
+      />
+      <div className="absolute top-[29px] left-[0px] w-80 h-[539px] overflow-hidden">
+        <div className="absolute top-[436.1px] left-[0px] w-80 h-[102.9px]">
+          <div className="absolute top-[0px] left-[0px] bg-whitesmoke w-80 h-[102.9px]" />
+          <div className="absolute top-[26.9px] left-[46.4px] flex flex-row items-start justify-start">
+            <div className="flex flex-row items-center justify-start gap-[20px]">
+              <img
+                className="relative w-[45.3px] h-[45.3px] object-cover"
+                alt=""
+                src="/key-1@2x.png"
+              />
+              <img
+                className="relative w-[40.6px] h-[40.6px] object-cover"
+                alt=""
+                src="/wheel@2x.png"
+              />
+              <img
+                className="relative w-[37.3px] h-[37.2px] object-cover"
+                alt=""
+                src="/fork-1@2x.png"
+              />
+              <img
+                className="relative w-11 h-11 object-cover"
+                alt=""
+                src="/divinggoggles-1@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[0px] left-[24.2px] w-[208.7px] h-[118.5px]">
+          <div className="absolute top-[0px] left-[8.8px] flex flex-col items-start justify-start gap-[10px]">
+            <div className="relative tracking-[0.04em]">Sedon</div>
+            <div className="relative tracking-[0.04em] text-gray-200">
+              tourism
+            </div>
+          </div>
+          <div className="absolute top-[73.5px] left-[0px] flex flex-row items-center justify-start gap-[8px]">
+            <img
+              className="relative w-[50.3px] h-[45px] object-cover"
+              alt=""
+              src="/pin-5-1@2x.png"
+            />
+            <div className="flex flex-row items-center justify-center gap-[20px]">
+              <img
+                className="relative w-[36.1px] h-[36.1px] object-cover"
+                alt=""
+                src="/phonecall-3-1@2x.png"
+              />
+              <img
+                className="relative w-[36.8px] h-[36.8px] object-cover"
+                alt=""
+                src="/whatsapp-2@2x.png"
+              />
+              <img
+                className="relative w-[37.5px] h-[37.5px] object-cover"
+                alt=""
+                src="/4834205-1@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-[138.3px] left-[0px] w-80 h-[297.8px] object-cover"
+          alt=""
+          src="/frame-108@2x.png"
+        />
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default Sedon
+export default Sedon;
