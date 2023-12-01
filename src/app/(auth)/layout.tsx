44f1eb1cfb8553ab90a80875b174fc997eb25622
Databayt <osmanabdout@hotmail.com>
2023-12-01 11:19:50 +0300

    1

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Rubik } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--inter" });
+const rubik = Rubik({ subsets: ["latin"], variable: "--inter" });
+
+export const metadata: Metadata = {
+  title: "Databayt",
+  description: "business automation",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={(inter.variable, rubik.variable)}>
+        
+        {children}
+        
+      </body>
+    </html>
+  );
+}
