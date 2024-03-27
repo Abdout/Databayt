b1fee10b4810ca1a63d8de6dcc64b23b1e15569e
Databayt <osmanabdout@hotmail.com>
2024-03-27 21:42:24 +0300

    8

--- src/component/atom/button/md.tsx
+++ src/component/atom/button/md.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MdButton = (props: {placeholder: string; weight?: number}) => {
   return (
-    <button className='bg-yellow-400 px-7 py-3 text-[{props.weight}px] font-medium tracking-wide hover:bg-[#ffde3b]'>
+    <button className='bg-yellow-400 px-7 py-3 text-[{props.weight}px] font-medium tracking-wide hover:bg-[#edc33a]'>
         {props.placeholder}
     </button>
   )
