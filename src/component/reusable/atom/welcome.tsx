8ea1d144ff7ce60096656b376154006db58b0d95
abdout <osmanabdout@hotmail.com>
2024-05-21 19:03:21 +0300

    x

--- src/component/reusable/atom/welcome.tsx
+++ src/component/reusable/atom/welcome.tsx
@@ -5,7 +5,7 @@ const Welcome = () => {
   return (
     <div className='flex flex-col px-60 space-y-6 items-start justify-center'>
       <Title title='Atom playground' description='Welcome to '/>
-      <p className='text-[16px] font-light pr-10'>You may explore the reusable UI components by selecting a component from the navigation menu above 👆. Components are categorized from basic to complex, following a chemistry-inspired hierarchy: Atom, Pound, Cell, and Organ.</p>
+      <p className='text-[16px] font-light pr-10 tracking-wider'>You may explore the reusable UI components by selecting a component from the navigation menu above 👆. Components are categorized from basic to complex, following a chemistry-inspired hierarchy: Atom, Pound, Cell, and Organ.</p>
     </div>
   )
 
