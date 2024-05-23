ffd1932e9bb4f87218175579408d292606c855b9
abdout <osmanabdout@hotmail.com>
2024-05-23 12:04:24 +0300

    x

--- src/component/reusable/logic/open.tsx
+++ src/component/reusable/logic/open.tsx
@@ -1,22 +1,22 @@
-'use client';
+// 'use client';
 
-import Modal from "../pound/modal";
-import { useModal } from "./modal";
+// import Modal from "../pound/modal";
+// import { useModal } from "./modal";
 
 
-const TryModal = () => {
-  const { modal } = useModal();
-  const extend = kit.find(kit => kit.id.toString() === modal.id);
-  return (
-    <>
-    {modal.open && extend && (
-     <Modal content={<Profile kit={extend} />} />
-    )}
-    <div className="p-4 grid grid-cols-4  gap-[90px] gap-y-28">
-      model
-    </div>
-    </>
-  );
-};
+// const TryModal = () => {
+//   const { modal } = useModal();
+//   const extend = kit.find(kit => kit.id.toString() === modal.id);
+//   return (
+//     <>
+//     {modal.open && extend && (
+//      <Modal content={<Profile kit={extend} />} />
+//     )}
+//     <div className="p-4 grid grid-cols-4  gap-[90px] gap-y-28">
+//       model
+//     </div>
+//     </>
+//   );
+// };
 
-export default TryModal;
+// export default TryModal;
